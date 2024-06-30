fdbe2cef28eeda261234410d1c5010aeeaa0b56d
Yahya Mohamed <yahya.hm750@gmail.com>
2024-06-30 19:55:13 +0000

    add multiplication and subtraction sections to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,12 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2)
+    } else if (gameType === "subtraction") {
+        displaySubtractionQuestion(num1, num2)
+    }  else if (gameType === "multiplication") {
+        displayMultiplicationQuestion(num1, num2)
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2)
     } else {
         alert(`Unknown Game Type: ${gametype}`)
         throw `Unknown Game Type: ${gametype}. Aborting!`
@@ -61,6 +67,12 @@ function calculateCorrectAnswer() {
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"]
+    } else if (operator === "-"){
+        return [operand1 - operand2, "addition"]
+    } else if (operator === "x"){
+        return [operand1 * operand2, "addition"]
+    } else if (operator === "/"){
+        return [operand1 / operand2, "addition"]
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}. Aborting`
@@ -88,8 +100,16 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+"
 }
 
-function displaySubtractionQuestion() {}
+function displaySubtractionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1
+    document.getElementById("operator").textContent = "-"
+}
 
-function displayMultiplicationQuestion() {}
+function displayMultiplicationQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1
+    document.getElementById("operand2").textContent = operand2
+    document.getElementById("operator").textContent = "x"
+}
 
 
