ade40d0f4bdb4a2a1ae80acb060862f2f01b9a72
Yahya Mohamed <yahya.hm750@gmail.com>
2024-06-30 14:31:25 +0000

    add set up for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame() {}
+
+function checkAnswe() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractionQuestion() {}
+
+function displayMultiplicationQuestion() {}
