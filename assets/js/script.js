c7934d5d7de4239d57766f9a00c8c7289ce80c67
Yahya Mohamed <yahya.hm750@gmail.com>
2024-07-01 14:50:50 +0000

    Add favicon and final tidy ups  for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed"
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1
     let num2 = Math.floor(Math.random() * 25) + 1
 
@@ -88,11 +92,15 @@ function incrementScore() {
     document.getElementById("score").innerText = ++oldScore
 }
 
+/**Gets the current incorrect score from the DOM and
+ * implements it by 1
+ */
 function incrementWrongAnswer() {
 
     let oldScore = parseInt(document.getElementById("incorrect").innerText)
     document.getElementById("incorrect").innerText = ++oldScore
 }
+
 
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1
